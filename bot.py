b9d3a93f292f1af616f7694096e088a2ae07890e
RodrigoProjects <RodrigoProjects>
2020-01-30 17:55:09 +0000

    Tratamento de erros de tipo ou empty lists | 2020-01-30 17:55

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-SITES = ['PCdiga']
+SITES = ['pcdiga']
 
 bot = commands.Bot(command_prefix='!')
 
@@ -42,6 +42,9 @@ async def get_links(ctx, site_name : str = None ):
         with codecs.open('pcdiga_links.txt','r','utf-8') as f:
             lines = f.readlines()
             res = '```\n'
+            if len(lines) == 0:
+                await ctx.send(f'Ficheiro de links **{site_name}** está vazio!')
+                return
             for num, line in enumerate(lines):
                 res += f'{num + 1} - {line[23:]}'
 
@@ -52,27 +55,36 @@ async def get_links(ctx, site_name : str = None ):
 
 @bot.command(name='rm_link', help='Comando que remove um link a ser usado numa loja')
 async def rm_link(ctx, site_name : str = None, line=None):
-    
-    line = int(line) if isinstance(line,int) else ctx.send('**O número de linhas tem de ser um inteiro!')
 
-    site_name = site_name.lower() if site_name is not None else site_name
-    if line == None:
+    if line == None and site_name == None:
+        await ctx.send('**Nome do site e número de linhas não fornecido!**')
+        return
+    elif line == None:
         await ctx.send('**Número de linhas não fornecido!**')
         return
+    
+    line = int(line) if line.isdigit() else await ctx.send('**O número de linhas tem de ser um inteiro!**')
 
-    if site_name == 'pcdiga':
-        with codecs.open('pcdiga_links.txt','r+','utf-8') as f:
-            lines = f.readlines()
-            f.seek(0)
-            for idx, line in enumerate(lines):
-                if idx + 1 != line:
-                    f.write(line) 
+    site_name = site_name.lower() if site_name is not None else site_name
+    
+    if site_name in SITES:
+        with codecs.open(f'{site_name}_links.txt','r','utf-8') as fr:
+            lines = fr.readlines()
+            if len(lines) == 0 or line > len(lines):
+                await ctx.send(f'Linha **{line}** não existe no ficheiro ou ficheiro vazio!')
+                return
+            line_removed = lines.pop(line - 1)
+        
+        with codecs.open(f'{site_name}_links.txt','w','utf-8') as fw:
+            for ln in lines:
+                fw.write(ln)
+
     else:
         sites = ', '.join(list(map(lambda strr: f'  -> **{strr}**\n', SITES)))
-        await ctx.send(f'**Nome do site em falta ou mal escrito!** (!get_links <nome_do_site>)\n*Opções:*\n{sites}')
+        await ctx.send(f'**Nome do site em falta ou mal escrito!** (!rm_link <nome_do_site> <linha>)\n*Opções:*\n{sites}')
         return
     
-    ctx.send("Linha **{line}** foi removida do ficheiro **{site_name}**.")
+    await ctx.send(f'Linha **{line}** - **{line_removed[23:]}** removida do ficheiro **{site_name}**.')
         
 
 
